9b8ed1f2d6036c5512290d0a6ea339cbc43a2fa1
gusfersevillano <49257062+gusfersevillano@users.noreply.github.com>
2020-06-17 12:42:26 -0500

    prueba2

---
+++ dist/models/Photo.js
@@ -0,0 +1,29 @@
+"use strict";
+/*
+
+import { Schema, model, Document } from 'mongoose'
+
+const schema = new Schema({
+    transaccion: String,
+    tipo: String,
+    precio: Number,
+    sector: String,
+    metros: Number,
+    caracteristicas: String,
+    vendedor: String,
+    imagePath: String
+});
+
+export interface IPhoto extends Document {
+    transaccion: string;
+    tipo: string;
+    precio: number;
+    sector: string;
+    metros: number;
+    caracteristicas: string;
+    vendedor: string;
+    imagePath: string
+}
+
+export default model<IPhoto>('Photo', schema);
+*/ 
