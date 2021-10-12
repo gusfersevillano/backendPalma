48ea9de0345aecf2304089e2f3066c5d19cfc9f6
gusfersevillano <49257062+gusfersevillano@users.noreply.github.com>
2021-10-12 09:18:31 -0500

    Update database.ts

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,19 @@
 import { Pool } from 'pg';
 
+/*
 export const pool = new Pool({
     host: 'us11.acugis-dns.com',
     port: 5432,
     user: 'geobigda',
     password: '290ygoZ5Fa',
     database: 'geobigda_tecnisoil01',
+});*/
+
+export const pool = new Pool({
+    host: '198.211.101.74',
+    port: 5432,
+    user: 'postgres',
+    password: 'palma1234',
+    database: 'Tecnisoil',
 });
 
